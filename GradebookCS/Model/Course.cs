14f0d36a021231f52b421deb93907204d5c77bd4
Jules Voltaire <javoltaire@wpi.edu>
2015-12-23 04:26:54 -0500

    Course class added

---
+++ GradebookCS/Model/Course.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradebookCS.Model
+{
+    public class Course
+    {
+        #region Attributes
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Constructors
+
+        #endregion
+    }
+}
