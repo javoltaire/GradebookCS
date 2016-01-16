7b7164ed81db3051ea4e328bc673128920c37ef8
Jules Voltaire <javoltaire@wpi.edu>
2016-01-16 10:48:53 -0500

    Auto Selecting text in TextBox when focused implemented

---
+++ GradebookCS/View/CustomControls/AutoSelectOnFocusTextBox.cs
@@ -0,0 +1,31 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace GradebookCS.View.CustomControls
+{
+    public class AutoSelectOnFocusTextBox : TextBox
+    {
+        #region Constructors
+        /// <summary>
+        /// Constructor to initialize this class
+        /// </summary>
+        public AutoSelectOnFocusTextBox() : base()
+        {
+            this.GotFocus += AutoSelectOnFocusTextBox_GotFocus;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Selects the text in the TextBox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AutoSelectOnFocusTextBox_GotFocus(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            this.SelectionStart = 0;
+            this.SelectionLength = this.Text.Length;
+        }
+        #endregion
+
+    }
+}
